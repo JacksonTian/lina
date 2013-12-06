fbd1f32426d5ec3e3592c4b90ebf017d00290ee5
Jackson Tian <shyvo1987@gmail.com>
2013-12-06 23:28:29 +0800

    启用栈来进行保存符号的成对状态

--- test/lexer.test.js
+++ test/lexer.test.js
@@ -6,7 +6,61 @@ describe('lexer/scan', function () {
     lexer.scan('1').should.be.eql(['1']);
   });
 
+  it('11', function () {
+    lexer.scan('11').should.be.eql(['11']);
+  });
+
+  it('0.1', function () {
+    lexer.scan('0.1').should.be.eql(['0.1']);
+  });
+
+  it('.1', function () {
+    lexer.scan('.1').should.be.eql(['.1']);
+  });
+
+  it('-1', function () {
+    lexer.scan('-1').should.be.eql(['-1']);
+  });
+
   it('(+ 1 1)', function () {
     lexer.scan('(+ 1 1)').should.be.eql(['(', '+', '1', '1', ')']);
+  });
+
+  it('(+ 1 (+ 2 3))', function () {
+    lexer.scan('(+ 1 (+ 2 3))').should.be.eql(['(', '+', '1', '(', '+', '2', '3', ')', ')']);
+  });
+
+  it('[1 1]', function () {
+    lexer.scan('[1 1]').should.be.eql(['[', '1', '1', ']']);
+  });
+
+  it('["1" "2"]', function () {
+    lexer.scan('["1" "2"]').should.be.eql(['[', '"1"', '"2"', ']']);
+  });
+
+  it('"1"', function () {
+    lexer.scan('"1"').should.be.eql(['"1"']);
+  });
+
+  it('\'', function () {
+    (function () {
+      lexer.scan('\'');
+    }).should.throw('Except \'.\n\'\n ^');
+  });
+
+  it('()', function () {
+    lexer.scan('()').should.be.eql(['(', ')']);
+  });
+
+  it('((', function () {
+    (function () {
+      lexer.scan('((');
+    }).should.throw('Except ).\n((\n  ^');
+  });
+
+  it(')', function () {
+    (function () {
+      lexer.scan(')');
+    }).should.throw('Unexcepted ).\n)\n^');
   });
 });
